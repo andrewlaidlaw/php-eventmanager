23730d803d18c5bf29cfa53f841aa3147f1f52f4
andrewlaidlaw <36501534+andrewlaidlaw@users.noreply.github.com>
2021-01-11 17:02:27 +0000

    Debug

--- debug.php
+++ debug.php
@@ -2,6 +2,7 @@
 
 echo "Hostname: " . $_ENV['database-hostname'] . "<br/>";
 echo "Username: " . $_ENV['database-user'] ."<br />";
+echo "Password: " . $_ENV['database-password'] ."<br />";
 
 echo "";
 
