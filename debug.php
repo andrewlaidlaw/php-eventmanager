1160069bb631e60b6b256e9c6451c848cb535f73
andrewlaidlaw <36501534+andrewlaidlaw@users.noreply.github.com>
2021-01-11 18:25:58 +0000

    Debug

--- debug.php
+++ debug.php
@@ -6,12 +6,6 @@ echo "Hostname: " . $_ENV['database-hostname'] . "<br/>";
 echo "Username: " . $_ENV['database-user'] ."<br />";
 echo "Password: " . $_ENV['database-password'] ."<br />";
 
-$con = mysqli_connect("mariadb-eventmanager", "userPGR", "Wok4FDdRA6xBoiGW", "attendees");
-	if (!$con) {
-		// Error message if connection fails
-		die('Could not connect: ' . mysqli_connect_error());
-	}
-
 $con=connect();
 disconnect($con);
 
